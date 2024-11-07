2a5dfa394a8625eef004d8388b71c31ed5cea2a4
mark-lutabi <mark.lutabi@mths.ca>
2024-11-07 10:40:50 -0500

    code completely

--- main.ts
+++ main.ts
@@ -1,8 +1,42 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
+ * Created by: Mark lutabi
+ * Created on: November,6
  * This program ...
 */
 
-basic.showString('Hello, World!')
+
+// variables
+let sprite: game.LedSprite = null
+let loopCounter = 0
+let movement = 0
+
+// setup
+basic.showIcon(IconNames.Happy)
+
+// when "A" is pressed, the pixels move around the perimeter of the Micro:bit
+input.onButtonPressed(Button.A, function () {
+
+    // setup
+    basic.clearScreen()
+    loopCounter = 0
+    sprite = game.createSprite(0, 0)
+    basic.pause(500)
+
+    // loop pixel to move around the Micro:bit
+    while (loopCounter < 4) {
+        movement = 0
+        while (movement < 4) {
+            sprite.move(1)
+            basic.pause(500)
+            movement++
+        }
+        sprite.turn(Direction.Right, 90)
+        loopCounter++
+    }
+
+    // delete sprite and show happy face at the end
+    sprite.delete()
+    basic.showIcon(IconNames.Happy)
+})
+
